1c03aaaf9b904129f394fd96eb5c5b11aea6ea7a
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-10-16 16:51:27 -0600

    Create initial file structure

--- app/index.js
+++ app/index.js
@@ -2,5 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import MovieIndex from './components/movieIndex';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App/>, document.getElementById('main'))
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, document.getElementById('main'))
