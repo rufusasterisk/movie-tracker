f22a9e43aaba4901e8e9d3c7168b66590e1624ad
Rufus <rufusasterisk@gmail.com>
2017-10-18 16:27:23 -0600

    Add login tab

--- app/components/Login/Login.js
+++ app/components/Login/Login.js
@@ -13,8 +13,37 @@ export default class Login extends Component {
     };
   }
 
-  generateLoginTab() {
+  handleChange(event, key) {
+    this.setState({
+      [key]: event.target.value
+    });
+  }
 
+
+
+  generateLoginTab() {
+    return (
+      <form>
+        <input
+          className='login-email'
+          onChange={this.handleChange.bind(this, 'email')}
+          placeholder='Email'
+          type='text'
+          value={this.state.email} />
+        <input
+          className='login-password'
+          onChange={this.handleChange.bind(this, 'password')}
+          placeholder='Password'
+          type='text'
+          value={this.state.password} />
+        <input
+          className='login-submit'
+          onClick={this.someNewHandler}
+          type='submit'
+          value='Login'
+        />
+      </form>
+    );
   }
 
   generateNewUserTab() {
@@ -25,8 +54,6 @@ export default class Login extends Component {
     const generatorReturn = this.state.displayedTab === 'login'
       ? this.generateLoginTab()
       : this.generateNewUserTab();
-
-    
     return (
       <div>
         {generatorReturn}
